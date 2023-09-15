52fbf2843dd38a5aa43a3f5ce218f8c642db4207
Ronan Dowling <ronan@dowlings.me>
2023-09-15 07:13:35 +0000

    Pushed some files around

--- .devcontainer/reset.sh
+++ .devcontainer/reset.sh
@@ -5,22 +5,29 @@ cd /workspace
 
 echo "Initializing Drupal Dev Container Demo."
 echo "🗑️  Deleting files ..."
-rm -rf logs data
-mkdir logs data
+rm -rf data
+mkdir -p data/logs
 
 echo "🗃️  Dropping database ..."
 echo 'DROP DATABASE IF EXISTS drupal10' | mariadb -h db --password=root
 
 echo "💧 Installing a fresh Drupal ..."
-cd root
-rm -rf vendor composer.lock
-cp -f /drupal/composer.json /workspace/root/composer.json
+cd drupal
+# rm -rf vendor composer.lock
+cp -f /workspace/src/composer.json /workspace/drupal/composer.json
 composer install
 
 echo "📝 Overwriting settings ..."
-cp -f /workspace/.devcontainer/settings.drupal10.php /workspace/root/web/sites/default/settings.php
+cp -f /workspace/.devcontainer/settings.php /workspace/drupal/web/sites/default/settings.php
 
-echo "📝 Adding custom code directories ..."
-# todo
+echo "📁 Adding custom code directories ..."
+ln -fs /workspace/src/modules /workspace/drupal/web/sites/default/modules
+ln -fs /workspace/src/themes /workspace/drupal/web/sites/default/themes
 
-echo "👉 http://localhost:8001 ..."
+cat << EOF
+
+
+👉 http://localhost:8001 👈
+
+
+EOF
